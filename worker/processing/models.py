4720a93e1f1829ebfc6fac8528624f100cf73782
alekLukanen <alukanen@icloud.com>
2019-11-04 19:38:02 -0600

    added base data models.

--- worker/processing/models.py
+++ worker/processing/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Providers:
+    ALPACA = 'ALPACA'
+
+
+class DataStages:
+    ORIGINAL = 'ORIGINAL'
+    CLEANED = 'CLEANED'
+    DERIVED = 'DERIVED'
+
+
+class DataStatus:
+    INITIALIZED = 'INITIALIZED'
+    INCOMPLETE = 'INCOMPLETE'
+    COMPLETE = 'COMPLETE'
+    FAILED_EXTERNALLY = 'FAILED_EXTERNALLY'
+    FAILED_INTERNALLY = 'FAILED_INTERNALLY'
+
+
+class Data(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    provider = models.TextField(null=True)
+    provider_resource_path = models.TextField(null=True)
+    file_format = models.TextField()
+    stage = models.TextField()
+    status = models.TextField(default=DataStatus.INITIALIZED)
+    location = models.TextField()
+
+
+class Symbol(models.Model):
+    """
+    A Symbol can have one to many EquityData objects pointing
+    to it each at various stages.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid)
+    symbol = models.TextField()
+    name = models.TextField()
+
+    def get_price(self, start_date, end_date, precision='1d'):
+        pass
+
+    def get_volume(self, start_date, end_date, precision='1d'):
+        pass
+
+
+class EquityData(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid)
+    exchange = models.TextField(null=True)
+    category = models.TextField(null=True)  # price, volume, financial info, description, etc...
+    start_date = models.DateTimeField(null=True)
+    end_date = models.DateTimeField(null=True)
+    data = models.ForeignKey(Data)
+    symbol = models.ForeignKey(Symbol)
